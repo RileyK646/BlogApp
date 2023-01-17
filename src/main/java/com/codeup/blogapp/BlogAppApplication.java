03c35af3e0ecef1c8cef7cf8aff07a3e19f09da7
RileyK646 <RileyKing646@gmail.com>
2023-01-17 10:38:14 -0500

    first commit

--- src/main/java/com/codeup/blogapp/BlogAppApplication.java
+++ src/main/java/com/codeup/blogapp/BlogAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAppApplication.class, args);
+    }
 
 }
--- src/test/java/com/codeup/blogapp/BlogAppApplicationTests.java
+++ src/test/java/com/codeup/blogapp/BlogAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BlogAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
